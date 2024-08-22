7d558bc521bbf953294812fdb67c00dccb61dd27
Ajay Ladwa <ladwa2000@gmail.com>
2024-08-22 20:49:25 +0100

    Update 6. Limit + Aliasing.sql

--- 6. Limit + Aliasing.sql
+++ 6. Limit + Aliasing.sql
@@ -7,17 +7,17 @@ LIMIT 3 # LIMIT specifies how many rows we want in our output, i.e. we want the 
 
 SELECT *
 FROM employee_demographics
-ORDER BY age DESC # We can also combine LIMIT statements with ORDER BY, like in this example where we use ORDER BY to output the top 3 oldest people and have them in descending order.
+ORDER BY age DESC # We can also combine LIMIT clauses with ORDER BY, like in this example where we use ORDER BY to output the top 3 oldest people and have them in descending order.
 LIMIT 3 
 ;
 
 SELECT *
 FROM employee_demographics
 ORDER BY age DESC
-LIMIT 2, 1  # Adding this additional parameter means we start from the 2nd position and look at the next one row after and output that result, i.e. the statement will start looking at 'age = 46', who is Donna Meagle, and look at the next one row after, which would be Leslie Knope, 'age = 44'. 
+LIMIT 2, 1  # Adding this additional parameter means we start from the 2nd position and look at the next one row after and output that result, i.e. the clause will start looking at 'age = 46', who is Donna Meagle, and look at the next one row after, which would be Leslie Knope, 'age = 44'. 
 ;
 
-# NB 1.0: What the above is essentially saying is starting from a certain position, i.e. the LIMIT statement, so in this case, the second position, the second parameter denotes the outputs after that position we want to have, in this case, the next one row, but if we change that to be '4', we're now saying we want to take the next 4 positions after the LIMIT. 
+# NB 1.0: What the above is essentially saying is starting from a certain position, i.e. the LIMIT clause, so in this case, the second position, the second parameter denotes the outputs after that position we want to have, in this case, the next one row, but if we change that to be '4', we're now saying we want to take the next 4 positions after the LIMIT. 
 
 # NB 1.1: The results will also be different depending on if the data has been sorted in either ascending or descending order before.
 
